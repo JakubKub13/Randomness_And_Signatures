3d13df376f355f098ca31e99d251507d443a4744
JK1331 <jakubkubala3@gmail.com>
2022-10-21 09:12:45 +0200

    example NotRandom contract

--- contracts/NotRandom.sol
+++ contracts/NotRandom.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.17;
 
 contract NotRandom {
     function getRandomNumber() public view returns (uint256 notRandom) {
-        
+        notRandom = uint256(blockhash(block.number - 1));
     }
     
     function tossCoin() public view returns (bool heads) {
-
+        heads = getRandomNumber() % 2 == 0;
     }
 }

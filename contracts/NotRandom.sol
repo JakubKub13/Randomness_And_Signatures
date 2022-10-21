b63681752929315344eab0b0250c7bd52aad3bec
JK1331 <jakubkubala3@gmail.com>
2022-10-21 08:51:23 +0200

    Preparing contracts

---
+++ contracts/NotRandom.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract NotRandom {
+    function getRandomNumber() public view returns (uint256 notRandom) {
+        
+    }
+    
+    function tossCoin() public view returns (bool heads) {
+
+    }
+}

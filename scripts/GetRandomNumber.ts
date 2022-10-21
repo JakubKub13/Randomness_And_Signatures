50f8a557ebdf405102e6495d5dc01dfb21293dcd
JK1331 <jakubkubala3@gmail.com>
2022-10-21 11:48:47 +0200

    Adding consol control panel to script

--- scripts/GetRandomNumber.ts
+++ scripts/GetRandomNumber.ts
@@ -2,6 +2,45 @@ import { ethers } from "hardhat";
 import * as readline from "readline";
 import { NotRandom } from "../typechain-types/NotRandom";
 
+async function main() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question(
+      "Select operation: \n Options: \n [1]: Random from block hash \n [2]: Toss a coin \n [3]: Message signature \n [4]: Random from a sealed seed \n [5]: Random from block hash plus a sealed seed \n [6]: Random from randao \n",
+      (answer) => {
+        console.log(`Selected: ${answer}`);
+        const option = Number(answer);
+        switch (option) {
+          case 1:
+            blockHashRandomness();
+            break;
+          case 2:
+            tossCoin();
+            break;
+          case 3:
+            signature();
+            break;
+          case 4:
+            sealedSeed();
+            break;
+          case 5:
+            randomSealedSeed();
+            break;
+          case 6:
+            randao();
+            break;
+          default:
+            console.log("Invalid");
+            break;
+        }
+        rl.close();
+      }
+    );
+  }
+  
+
 async function blockHashRandomness() {
     const contractFactory = await ethers.getContractFactory("NotRandom");
     contractFactory.deploy().then(async (result) => {
@@ -231,7 +270,7 @@ async function blockHashRandomness() {
     });
   }
 
-  randao().catch((error) => {
+  main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
